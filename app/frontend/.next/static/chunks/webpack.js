9473962c12df9b99c461b9d100799b6b4c5fb993
wisdom nwakaku <nwakakucc@gmail.com>
2023-03-08 10:32:21 +0100

    mastercraft

--- app/frontend/.next/static/chunks/webpack.js
+++ app/frontend/.next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "564f68c374a17f07"; }
+/******/ 		__webpack_require__.h = function() { return "b5fdcfe2238f65df"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/frontend/pages/inheritance/index.js
+++ app/frontend/pages/inheritance/index.js
@@ -43,7 +43,7 @@ function Index() {
     console.log(myNFTs)
     const amount = myNFTs?.amount?.toString();
     const tokenId = myNFTs?.tokenId?.toNumber();
-    if (tokenId == 0) setTokenType("ERC20");
+    if (tokenId == 0){ setTokenType("ERC20")}
     else {
       if (amount == 0) setTokenType("ERC721");
       else setTokenType("ERC1155");
@@ -58,54 +58,58 @@ function Index() {
           <Header />
           <div></div>
           <hr></hr>
-          
+
           <main className="flex flex-col items-center justify-center grow mx-5">
-             {isConnected ? (
-          <>
-            <h2 className="text-3xl font-semibold max-w-50 justify-center">
-             Check Your Inheritance
-            </h2>
+            {isConnected ? (
+              <>
+                <h2 className="text-3xl font-semibold max-w-50 justify-center">
+                  Check Your Inheritance
+                </h2>
 
-            <p className="text-xl mt-2 text-gray-400 border-b-2 border-gray-300 pb-3 justify-center">
-              This is the Assumed Inheritance you will be recieving from this Account
-            </p>
+                <p className="text-xl mt-2 text-gray-400 border-b-2 border-gray-300 pb-3 justify-center">
+                  This is the Assumed Inheritance you will be recieving from
+                  this Account
+                </p>
 
-            <div className="grid grid-cols-6 grid-row-flow gap-4 w-full mx-auto mt-5 ml-5 justify-items-center">
-              <h3 className="font-semibold">Token</h3>
-              <h3 className="font-semibold">Beneficiary</h3>
-              <h3 className="font-semibold">Amount</h3>
-              <h3 className="font-semibold">Token ID</h3>
-              <h3 className="font-semibold">Type</h3>
-              <h3 className="font-semibold">Delete</h3>
-            </div>
-            {myNFTs.map((legacy, index) => (
-              <div className="grid grid-cols-6 grid-row-flow gap-4 w-full mx-auto mt-5 ml-5">
-                <h3 className="truncate">{legacy?.token?.toString()}</h3>
-                <h3 className="truncate">{legacy?.beneficiary?.toString()}</h3>
-                {tokenType == "ERC20" ? (
-                  <h3 className="justify-self-center truncate">
-                    {ethers.utils.formatEther(legacy?.amount).toString()}
-                  </h3>
-                ) : (
-                  <h3 className="justify-self-center">
-                    {legacy?.amount?.toString()}
-                  </h3>
-                )}
-                <h3 className="justify-self-center">
-                  {legacy?.tokenId?.toString()}
-                </h3>
-                <h3 className="justify-self-center">{tokenType}</h3>
-                <button
-                  className="justify-self-center text-white bg-black hover:bg-red-500 text-bold rounded-full px-6 sm:w-auto"
-                  onClick={() => removeLegacy()}
-                >
-                  X
-                </button>
-              </div>
-            ))}
-          </>): <>
-            snull</>
-          }
+                <div className="grid grid-cols-6 grid-row-flow gap-4 w-full mx-auto mt-5 ml-5 justify-items-center">
+                  <h3 className="font-semibold">Token</h3>
+                  <h3 className="font-semibold">Beneficiary</h3>
+                  <h3 className="font-semibold">Amount</h3>
+                  <h3 className="font-semibold">Token ID</h3>
+                  <h3 className="font-semibold">Type</h3>
+                  <h3 className="font-semibold">Delete</h3>
+                </div>
+                {myNFTs.map((legacy, index) => (
+                  <div className="grid grid-cols-6 grid-row-flow gap-4 w-full mx-auto mt-5 ml-5">
+                    <h3 className="truncate">{legacy?.token?.toString()}</h3>
+                    <h3 className="truncate">
+                      {legacy?.beneficiary?.toString()}
+                    </h3>
+                    {tokenType == "ERC20" ? (
+                      <h3 className="justify-self-center truncate">
+                        {ethers.utils.formatEther(legacy?.amount).toString()}
+                      </h3>
+                    ) : (
+                      <h3 className="justify-self-center">
+                        {ethers.utils.formatEther(legacy?.amount).toString()}
+                      </h3>
+                    )}
+                    <h3 className="justify-self-center">
+                      {legacy?.tokenId?.toString()}
+                    </h3>
+                    <h3 className="justify-self-center">{tokenType}</h3>
+                    <button
+                      className="justify-self-center text-white bg-black hover:bg-red-500 text-bold rounded-full px-6 sm:w-auto"
+                      onClick={() => removeLegacy()}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <>snull</>
+            )}
           </main>
         </div>
       )}
